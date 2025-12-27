90520f5edac5cca4df5390331f07a06e6fb8b117
kingkingng2222-prog <kingkingng2222@gmail.com>
2025-12-27 21:12:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-document.addEventListener("DOMContentLoaded", function () {
+alert("SCRIPT LOADED");
 
-  document.querySelectorAll("svg path").forEach(state => {
-
-    state.addEventListener("click", function () {
-      alert("Clicked state ID: " + this.id);
-    });
-
-    state.addEventListener("mouseenter", function () {
-      this.style.fill = "#90caf9";
-      this.style.cursor = "pointer";
-    });
-
-    state.addEventListener("mouseleave", function () {
-      this.style.fill = "#cfd8dc";
-    });
-
-  });
-
-});
+document.querySelector("#WB").onclick = function () {
+  alert("WB CLICKED");
+};
